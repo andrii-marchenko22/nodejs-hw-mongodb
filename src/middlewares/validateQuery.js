2b895e057741fcd6d96d0fef412a6c1e5d1f8af3
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-08-17 15:10:32 +0300

    added allowUnknown

--- src/middlewares/validateQuery.js
+++ src/middlewares/validateQuery.js
@@ -3,6 +3,7 @@ import createHttpError from 'http-errors';
 export const validateQuery = (schema) => async (req, res, next) => {
   try {
     const value = await schema.validateAsync(req.query, {
+      allowUnknown: false,
       abortEarly: false,
       convert: true,
     });
