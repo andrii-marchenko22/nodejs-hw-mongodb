016b2684cd0a2faab9ca85a10481752e3c0136b5
Andrii Marchenko <marchenko22andrey@gmail.com>
2025-08-23 17:27:24 +0300

    added Schema

---
+++ src/db/models/user.js
@@ -0,0 +1,27 @@
+import { Schema, model } from 'mongoose';
+
+export const userSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model('user', userSchema);
